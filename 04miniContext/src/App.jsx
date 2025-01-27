973995c39eddc0a52b49e96c5af649528af0697e
Sanjana <sanjanahlg@gmail.com>
2025-01-27 11:21:32 +0530

    Context API

---
+++ 04miniContext/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/ContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+
+    </UserContextProvider>
+    
+    </>
+    
+  )
+}
+
+export default App
